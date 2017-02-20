a3014f869d9c1c295457b298e165d35d209762a3
nadraliev <nadraliev@gmail.com>
2017-02-20 14:31:35 +0300

    добавил AppComponent

---
+++ app/src/main/java/com/soutvoid/gamesproject/app/dagger/AppComponent.java
@@ -0,0 +1,15 @@
+package com.soutvoid.gamesproject.app.dagger;
+
+import com.agna.ferro.mvp.component.scope.PerApplication;
+
+import dagger.Component;
+
+/**
+ * Created by andrew on 2/20/17.
+ */
+@PerApplication
+@Component(modules = {
+
+})
+public interface AppComponent {
+}
