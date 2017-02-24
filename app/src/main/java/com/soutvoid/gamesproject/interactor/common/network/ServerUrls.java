f083b0520d4c788b59e754991a535df33da66b87
nadraliev <nadraliev@gmail.com>
2017-02-24 13:01:46 +0300

    добавил url для всех эндпоинтов

--- app/src/main/java/com/soutvoid/gamesproject/interactor/common/network/ServerUrls.java
+++ app/src/main/java/com/soutvoid/gamesproject/interactor/common/network/ServerUrls.java
@@ -28,4 +28,28 @@ public class ServerUrls {
 
     //genres
     public static final String GENRES_URL = "genres/";
+
+    //keywords
+    public static final String KEYWORDS_URL = "keywords/";
+
+    //people
+    public static final String PEOPLE_URL = "people/";
+
+    //platforms
+    public static final String PLATFORMS_URL = "platforms/";
+
+    //player perspectives
+    public static final String PLAYER_PERSPECTIVES_URL = "player_perspectives/";
+
+    //pulses
+    public static final String PULSES_URL = "pulses/";
+
+    //release dates
+    public static final String RELEASE_DATES_URL = "release_dates/";
+
+    //reviews
+    public static final String REVIEWS_URL = "reviews/";
+
+    //themes
+    public static final String THEMES_URL = "themes/";
 }
