71f57506b71bb5657543d90b655b20528092174e
nadraliev <nadraliev@gmail.com>
2017-03-20 14:36:04 +0300

    добавил примеров на экран

--- app/src/main/java/com/soutvoid/gamesproject/ui/screen/main/MainActivityPresenter.java
+++ app/src/main/java/com/soutvoid/gamesproject/ui/screen/main/MainActivityPresenter.java
@@ -9,6 +9,8 @@ import com.soutvoid.gamesproject.interactor.util.OrderBuilder;
 import com.soutvoid.gamesproject.ui.base.activity.BasePresenter;
 import com.soutvoid.gamesproject.ui.common.error.ErrorHandler;
 
+import java.util.HashMap;
+
 import javax.inject.Inject;
 
 
@@ -30,17 +32,32 @@ public class MainActivityPresenter extends BasePresenter<MainActivityView> {
         FieldsBuilder fieldsBuilder = new FieldsBuilder();
         OrderBuilder orderBuilder = new OrderBuilder();
         FilterBuilder filterBuilder = new FilterBuilder();
+
         subscribeNetworkQuery(gameRepository.searchGames(
-                "Portal",
+                null,
                 fieldsBuilder.addAllFields().build(),
                 20,
                 0,
                 orderBuilder.addField(GameFields.POPULARITY).build(),
                 filterBuilder.setField(GameFields.FIRST_RELEASE_DATE).setFactor(FilterBuilder.Factor.gt).setValue("1483228800000").buildMap()
-        ), games -> {
-            getView().onAddExploreSetView("Fresh popular", games);
-        }, throwable -> {
+        ), games -> getView().onAddExploreSetView("Fresh popular", games));
 
-        });
+        subscribeNetworkQuery(gameRepository.searchGames(
+                null,
+                fieldsBuilder.clear().addAllFields().build(),
+                20,
+                0,
+                orderBuilder.addField(GameFields.FIRST_RELEASE_DATE).build(),
+                new HashMap<String, String>()
+        ), games -> getView().onAddExploreSetView("Just came out", games));
+
+        subscribeNetworkQuery(gameRepository.searchGames(
+                null,
+                fieldsBuilder.clear().addAllFields().build(),
+                20,
+                0,
+                orderBuilder.clear().addField(GameFields.POPULARITY).build(),
+                filterBuilder.clear().setField(GameFields.GENRES).setFactor(FilterBuilder.Factor.in).setValue("2").buildMap()   //TODO make enum for genres
+        ), games -> getView().onAddExploreSetView("Point-and-click", games));
     }
 }
