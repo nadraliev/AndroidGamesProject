258911680dcc69a3907515d7f81e41fc8ad8e7df
nadraliev <nadraliev@gmail.com>
2017-05-06 19:32:34 +0300

    добавил загрузку showcase на главном экране

--- app/src/main/java/com/soutvoid/gamesproject/ui/screen/main/MainActivityPresenter.java
+++ app/src/main/java/com/soutvoid/gamesproject/ui/screen/main/MainActivityPresenter.java
@@ -1,7 +1,12 @@
 package com.soutvoid.gamesproject.ui.screen.main;
 
 import com.agna.ferro.mvp.component.scope.PerScreen;
+import com.soutvoid.gamesproject.domain.game.fields.GameFields;
 import com.soutvoid.gamesproject.interactor.game.GameRepository;
+import com.soutvoid.gamesproject.interactor.util.Fields;
+import com.soutvoid.gamesproject.interactor.util.Filter;
+import com.soutvoid.gamesproject.interactor.util.Order;
+import com.soutvoid.gamesproject.interactor.util.Query;
 import com.soutvoid.gamesproject.ui.base.activity.BasePresenter;
 import com.soutvoid.gamesproject.ui.common.error.ErrorHandler;
 
@@ -31,6 +36,19 @@ public class MainActivityPresenter extends BasePresenter<MainActivityView> {
     public void onLoad(boolean viewRecreated) {
         super.onLoad(viewRecreated);
 
+        //main showcase query
+        Query query = new Query(
+                "Showcase",
+                null,
+                Fields.builder().build(),
+                20,
+                0,
+                Order.builder().field(GameFields.POPULARITY.toString()).build(),
+                Filter.builder().field(GameFields.FIRST_RELEASE_DATE.toString()).factor(Filter.Factor.gt.toString()).value("1483228800000").build()
+        );
+
+        subscribeNetworkQuery(gameRepository.searchGames(query),
+                games -> getView().onSetShowcaseViewGames(games));
     }
 
     private void dataLoaded() {
